3b148c8110c532a6551b82256bdcd42d4f1f2e52
AmineZhioua <amine-zhioua@outlook.com>
2024-11-19 17:36:21 -0800

    new commit

--- components/EventForm.tsx
+++ components/EventForm.tsx
@@ -10,6 +10,10 @@ import Dropdown from "./shared/Dropdown"
 import { Textarea } from "@/components/ui/textarea"
 import FileUploader from "./shared/FileUploader"
 import { useState } from "react"
+import Image from 'next/image'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Checkbox } from "./ui/checkbox"
  
 const eventFormSchema = z.object({
   title: z.string().min(10, { message: "Title must be at least 10 characters" }),
@@ -70,6 +74,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
     return (
         <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+                {/* Event Title Input */}
                 <FormField
                     control={form.control}
                     name="title"
@@ -83,6 +89,8 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         </FormItem>
                     )}
                 />
+
+                {/* Event Category Select */}
                 <FormField
                     control={form.control}
                     name="categoryId"
@@ -97,6 +105,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                     )}
                 />
 
+                {/* Event Description Input */}
                 <FormField
                     control={form.control}
                     name="description"
@@ -111,6 +120,7 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                     )}
                 />
 
+                {/* Event Image Picker */}
                 <FormField
                     control={form.control}
                     name="imageUrl"
@@ -124,7 +134,139 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         </FormItem>
                     )}
                 />
-                <Button type="submit">Submit</Button>
+
+                {/* Event Location Input */}
+                <FormField
+                    control={form.control}
+                    name="location"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Location</FormLabel>
+                            <FormControl>
+                                <div className="flex items-center relative">
+                                    <Image
+                                        src="/assets/icons/location.svg"
+                                        alt="location"
+                                        className="absolute left-[10px]"
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <Input placeholder="Enter event location or make it online" className="px-10" {...field} />
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                {/* Event Start Date Input */}
+                <FormField
+                    control={form.control}
+                    name="startDateTime"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Event Start Date</FormLabel>
+                            <FormControl className="w-full">
+                                <div className="flex items-center relative w-full">
+                                    <Image
+                                        src="/assets/icons/calendar.svg"
+                                        alt="calendar"
+                                        className="absolute z-10 left-[10px]"
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <DatePicker 
+                                        className="relative w-full border px-5 pl-10 py-2 rounded-md"
+                                        selected={field.value} 
+                                        onChange={(date) => field.onChange(date)} 
+                                        showTimeSelect
+                                        timeInputLabel="Time:"
+                                        dateFormat="MM/dd/yyyy h:mm aa"
+                                        wrapperClassName="w-full"
+                                    />
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                {/* Event End Date Input */}
+                <FormField
+                    control={form.control}
+                    name="endDateTime"
+                    render={({ field }) => (
+                        <FormItem className="w-full">
+                            <FormLabel>Event End Date</FormLabel>
+                            <FormControl>
+                                <div className="flex justify-center items-center relative">
+                                    <Image
+                                        src="/assets/icons/calendar.svg"
+                                        alt="calendar"
+                                        className="absolute z-10 left-[10px]"
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <DatePicker 
+                                        className="relative w-full border px-5 pl-10 py-2 rounded-md"
+                                        selected={field.value} 
+                                        onChange={(date) => field.onChange(date)} 
+                                        showTimeSelect
+                                        timeInputLabel="Time:"
+                                        dateFormat="MM/dd/yyyy h:mm aa"
+                                        wrapperClassName="w-full"
+                                    />
+                                </div>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+
+                {/* Event Price Input */}
+                <FormField
+                    control={form.control}
+                    name="price"
+                    render={({ field }) => (
+                        <FormItem className="w-full">
+                            <FormLabel>Price</FormLabel>
+                            <FormControl>
+                                <div className="flex items-center relative">
+                                    <Image
+                                        src="/assets/icons/money.svg"
+                                        alt="location"
+                                        className="absolute left-[10px]"
+                                        width={24}
+                                        height={24}
+                                    />
+                                    <Input className="pl-10" type="number" placeholder="Enter a price" {...field} />
+                                    {/* Event isFree input */}
+                                    <FormField
+                                        control={form.control}
+                                        name="isFree"
+                                        render={({ field }) => (
+                                            <FormItem className="w-full absolute">
+                                                <FormControl>
+                                                    <div className="flex items-center flex-row-reverse">
+                                                        <p className="mx-2">Free</p>
+                                                        <Checkbox 
+                                                            onCheckedChange={field.onChange} 
+                                                            checked={field.value} 
+                                                            id="isFree" 
+                                                        />
+                                                    </div>
+                                                </FormControl>
+                                                <FormMessage />
+                                            </FormItem>
+                                        )}
+                                    />
+                                </div>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
+
+                <Button type="submit" className="w-full">Submit</Button>
             </form>
     </Form>
     );
--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -1,4 +1,10 @@
-import React, { Dispatch, SetStateAction } from 'react'
+import { convertFileToUrl } from '@/lib/utils'
+import React, { Dispatch, SetStateAction, useCallback } from 'react'
+import {useDropzone} from 'react-dropzone'
+import { FileWithPath } from 'react-dropzone'
+import { generateClientDropzoneAccept } from 'uploadthing/client'
+import { Button } from '../ui/button'
+import Image from 'next/image'
 
 type FileUploaderProps = {
     onFieldChange: (value: string) => void,
@@ -6,14 +12,43 @@ type FileUploaderProps = {
     setFiles: Dispatch<SetStateAction<File[]>>
 }
 
-
 const FileUploader = ({ onFieldChange, imageUrl, setFiles }: FileUploaderProps) => {
-    return (
-        <div>FileUploader</div>
-    );
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        setFiles(acceptedFiles);
+        onFieldChange(convertFileToUrl(acceptedFiles[0]));
+    }, [])
+
+    const { getRootProps, getInputProps } = useDropzone({
+        onDrop,
+        accept: "image/*" ? generateClientDropzoneAccept(['image/*']) : undefined,
+    });
+
+  return (
+    <div {...getRootProps()} className='flex justify-center text-center border'>
+      <input {...getInputProps()} />
+      {imageUrl ? (
+        <div className="flex h-full w-full flex-1 justify-center">
+          <Image
+            src={imageUrl}
+            alt="image"
+            width={250}
+            height={250}
+            className="w-full object-cover object-center"
+          />
+        </div>
+      ) : (
+        <div className="flex-center justify-center items-center flex-col py-5 text-grey-500">
+          <Image src="/assets/icons/upload.svg" width={50} height={50} alt="file upload" className='relative left-1/2 -translate-x-1/2' />
+          <h3 className="mb-2 mt-2">Drag photo here</h3>
+          <p className="p-medium-12 mb-4">SVG, PNG, JPG</p>
+          <Button type="button" className="rounded-full">
+            Select from computer
+          </Button>
+        </div>
+      )}
+    </div>
+  )
 }
-
-
 
 
 export default FileUploader;
