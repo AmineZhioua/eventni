ddb3e18c2edfad636aba167f52f2a62d45e64f34
AmineZhioua <amine-zhioua@outlook.com>
2024-12-08 15:27:02 -0800

    Used Link instead of <a>

--- components/CheckoutButton.tsx
+++ components/CheckoutButton.tsx
@@ -4,6 +4,7 @@ import { SignedIn, SignedOut, useUser } from '@clerk/nextjs';
 import React from 'react'
 import { Button } from './ui/button';
 import Checkout from './Checkout';
+import Link from 'next/link';
 
 
 const CheckoutButton = ({ event }: { event: IEvent }) => {
@@ -23,9 +24,9 @@ const CheckoutButton = ({ event }: { event: IEvent }) => {
                 <>
                     <SignedOut>
                         <Button className='flex w-full items-center justify-center rounded-md border border-transparent px-8 py-3 text-base font-medium text-white hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-700 focus:ring-offset-2'>
-                            <a href='/sign-in'>
+                            <Link href='/sign-in'>
                                 <i className="fa-solid fa-plus"></i> Join Now
-                            </a>
+                            </Link>
                         </Button>
                     </SignedOut>
 
